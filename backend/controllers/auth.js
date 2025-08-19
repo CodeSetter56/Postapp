1fb024ffb7b0b15146881202f5016cbde03e6e1b
tigur da kodar <Codesetter56@gmail.com>
2025-08-19 20:32:52 +0530

    backend deployment 2

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -45,6 +45,10 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  });
   res.json({ message: "User logged out successfully" });
 };
